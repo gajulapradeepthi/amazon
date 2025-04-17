91273c75e4d5fe2cf5bb87a78827f49985f69098
gajulapradeepthi <55346535+gajulapradeepthi@users.noreply.github.com>
2025-04-18 01:17:03 +0530

    Update undeploy-deploy.sh

--- undeploy-deploy.sh
+++ undeploy-deploy.sh
@@ -1,29 +1,7 @@
-
-
-import sys
-appname1=sys.argv[1]
-apppath='/u01/app/oracle/middleware/applications/'
-def wlDeployUndeploy(username, password, adminURL, appName, location, targets, stageMode):
-    try:
-        #connect to admin server
-        connect(username, password, adminURL)
-        #start edit operation
-        edit()
-        startEdit()
-
-        #stop application
-        stopApplication(appName)
-
-        #start undeploying application to "mycluster" server
-        progress = undeploy(appName, timeout=60000)
-        progress.printStatus()
-        save()
-        activate(20000,block="true")
-        #start deploying application to ""mycluster" server
-        progress = deploy(appName,location,targets)
-        progress.printStatus()
-        #print 'Done deploying application' +appname
-
-    except Exception, e:
-         print ex.toString()
-wlDeployUndeploy('weblogic','weblogic1','t3://192.168.0.137:7001', appname1, apppath + appname1 + ".war", targets='mycluster', stageMode='stage')
+#!/bin/bash
+IFS=','
+SCRIPT_PATH=/u01/app/oracle/middleware/devops/scripts
+for i in $Application
+do
+/u01/app/oracle/middleware/wls12c/oracle_common/common/bin/wlst.sh $SCRIPT_PATH/undeploy-deploy.py $i
+done
