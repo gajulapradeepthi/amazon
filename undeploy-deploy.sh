3cbd397065cbbba0ca50a734df9bcdf081ea5b49
gajulapradeepthi <gajula.pradeepthi@gmail.com>
2025-04-19 00:27:49 +0530

    changes

--- undeploy-deploy.sh
+++ undeploy-deploy.sh
@@ -4,4 +4,6 @@ SCRIPT_PATH=/u01/app/oracle/middleware/devops/scripts
 for i in $Application
 do
 /u01/app/oracle/middleware/wls12c/oracle_common/common/bin/wlst.sh $SCRIPT_PATH/undeploy-deploy.py $i
+
+
 done
